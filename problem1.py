774b526939fd65f727f85f80c537669ce0021356
suyesh bhandari <suyeshb@gmail.com>
2014-06-05 20:28:15 -0400

    Solved problem 1

---
+++ problem1.py
@@ -0,0 +1,10 @@
+def divisible_by(num1, num2):
+	numbers = []
+	for num in xrange(1,1000):
+		if num % num1 == 0 or num % num2 == 0:
+			numbers.append(num)
+		
+	return sum(numbers)
+
+print divisible_by(3,5)
+
